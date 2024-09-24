4481345dd42c8fb94ed881fe12e522ef66beb529
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-09-24 11:13:46 -0400

    subir

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("get ok");
+});
+
+app.post("/", (req, res) => {
+  const data = req.body;
+  res.send(`Rota post: ${JSON.stringify(data)}`);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
