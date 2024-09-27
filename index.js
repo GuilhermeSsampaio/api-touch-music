921f542b25759228c658e3de30a423a789697354
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-09-27 09:51:20 -0400

    buscando blobs

--- index.js
+++ index.js
@@ -1,51 +1,83 @@
-const express = require("express"); // Importa o módulo Express
-const helmet = require("helmet"); // Importa o módulo Helmet para segurança
-const morgan = require("morgan"); // Importa o módulo Morgan para logging
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const app = express(); // Cria uma instância do aplicativo Express
-app.set("views-engine", "ejs");
+const app = express();
+app.engine("ejs", require("ejs").__express);
 app.set("views", "./app/views");
-const port = process.env.PORT || 3000; // Define a porta do servidor, usando a variável de ambiente PORT ou 3000 como padrão
+app.set("view engine", "ejs");
+const port = process.env.PORT || 3000;
 
-app.use(express.json()); // Middleware para parsear requisições com JSON
-app.use(helmet()); // Middleware para adicionar cabeçalhos de segurança HTTP
-app.use(morgan("dev")); // Middleware para logging de requisições HTTP no console
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.render("home/home.ejs");
-  console.log("acessando home"); // Loga no console que a rota GET foi acessada
+// Configurar Content Security Policy
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; media-src 'self' https://public-blob.squarecloud.dev"
+  );
+  next();
+});
+
+// Configurar Cross-Origin-Embedder-Policy
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+  next();
+});
+
+const apiKey = process.env.SQUARE_API_KEY;
+
+app.get("/", async (req, res) => {
+  try {
+    const fetch = (await import("node-fetch")).default;
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: process.env.SQUARE_API_KEY,
+      },
+    };
+
+    const response = await fetch(
+      "https://blob.squarecloud.app/v1/objects",
+      options
+    );
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.statusText}`);
+    }
+    const data = await response.json();
+
+    res.render("home/home.ejs", { data, apiKey });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erro ao buscar dados da API");
+  }
 });
 
 app.get("/musics", (req, res) => {
   res.render("music/musics.ejs");
-  console.log("acessando musicas");
 });
 
 app.get("/add_music", (req, res) => {
   res.render("music/music_form.ejs");
-  console.log("form add music acessado");
 });
 
 app.post("/", (req, res) => {
-  // Define uma rota POST para o caminho raiz
-  const data = req.body; // Obtém os dados do corpo da requisição
+  const data = req.body;
 
   if (!data || typeof data !== "object") {
-    // Verifica se os dados são válidos
-    return res.status(400).send({ error: "Invalid data" }); // Responde com status 400 e mensagem de erro se os dados forem inválidos
+    return res.status(400).send({ error: "Invalid data" });
   }
 
-  res.send(`Rota post: ${JSON.stringify(data)}`); // Responde com os dados recebidos em formato JSON
+  res.send(`Rota post: ${JSON.stringify(data)}`);
 });
 
-// Middleware de tratamento de erros
 app.use((err, req, res, next) => {
-  // Middleware para capturar erros
-  console.error(err.stack); // Loga o stack trace do erro no console
-  res.status(500).send("Algo deu errado!"); // Responde com status 500 e mensagem de erro
+  console.error(err.stack);
+  res.status(500).send("Algo deu errado!");
 });
 
 app.listen(port, () => {
-  // Inicia o servidor na porta definida
-  console.log(`Servidor rodando na porta ${port}`); // Loga no console que o servidor está rodando
+  console.log(`Servidor rodando na porta ${port}`);
 });
